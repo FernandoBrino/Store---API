89f75049cfe192217bea8cf83bf660d6883baf52
Fernando Brino <fernandobrino11@gmail.com>
2021-12-29 18:50:38 -0300

    Sort / Select fields and filter by name refactored

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -15,12 +15,30 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
   if (name) {
-    queryObject.name = name;
+    queryObject.name = { $regex: name, $options: "i" }; //$option: 'i', the 'i' insensitivity, will match upper and lower cases
+    //$regex, will match the string with query
   }
   //if the query exists will put in queryObject returning correspondent data, otherwise
   //queryObject will be an empty object, returning all objects
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+
+  //select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
